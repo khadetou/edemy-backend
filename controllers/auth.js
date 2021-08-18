6f2c80e8c7b651c627a1d10b3f50f13c6c426211
khadetou <khadetou96@gmail.com>
2021-08-18 21:08:21 +0000

    axios post

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 
 export const register = asyncHandler(async (req, res) => {
+  console.log(req.body);
   res.send("This is the authentication part");
 });
--- server.js
+++ server.js
@@ -16,16 +16,28 @@ const { PORT, NODE_ENV } = process.env;
 //connection to the database.
 connectDB();
 
-//CRUD Routing generated authomatically
-fs.readdirSync("./routes").map((route) =>
-  app.use("/api", require(`./routes/${route}`))
-);
-
-//Neutral expression.
-
 //Allow us to take data from the body for the authentification
 //Should be placed before our requests
 app.use(express.json({ extended: false }));
+
+//CRUD Routing generated authomatically
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method == "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+
+  next();
+});
+
+fs.readdirSync("./routes").map((route) =>
+  app.use("/api", require(`./routes/${route}`))
+);
 
 //Show us the actions that we hit
 if (NODE_ENV === "development") {
