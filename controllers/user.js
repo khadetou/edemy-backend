7bab35315c3bf91e87c1c9fdebd5e4b90609e3dd
khadetou <khadetou96@gmail.com>
2021-09-06 16:30:49 +0000

    course model

--- controllers/user.js
+++ controllers/user.js
@@ -3,5 +3,6 @@ import asyncHandler from "express-async-handler";
 
 export const user = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user.id).select("-password");
+
   res.json(user);
 });
